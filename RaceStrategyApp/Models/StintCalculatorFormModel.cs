4e8197df9b9041ac8aef2800eb8e11546d8fc486
RicardoJorge <ricardo.ricjose@gmail.com>
2024-03-05 23:15:12 +0000

    feat: Adds stint calculator page (#9)

---
+++ RaceStrategyApp/Models/StintCalculatorFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RaceStrategyApp.Models
+{
+    public class StintCalculatorFormModel
+    {
+        public bool HasFormationLap { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int LapMinutes { get; set; }
+        public int LapSeconds { get; set; }
+        public int LapMiliseconds { get; set; }
+        public double LitersPerLap { get; set; }
+        public int RaceHours { get; set; }
+        public int RaceMinutes { get; set; }
+        public int StintLimit { get; set; }
+        public int TankSize { get; set; }
+
+        public StintCalculatorFormModel() { }
+    }
+}
--- RaceStrategyApp/Program.cs
+++ RaceStrategyApp/Program.cs
@@ -10,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<IFuelCalculatorService, FuelCalculatorService>();
+builder.Services.AddSingleton<IStintCaluclatorService, StintCalculatorService>();
 
 await builder.Build().RunAsync();
